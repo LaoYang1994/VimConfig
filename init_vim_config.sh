93f2f5d36206d5742f9e69d79583f93975911924
LaoYang1994 <lhy_ustb@pku.edu.cn>
2020-01-08 02:41:50 +0800

    add init scripts

---
+++ init_vim_config.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Install vim configurations..."
+
+echo "cd to .vim directory"
+cd ~/.vim
+
+echo "backup origin vimrc..."
+if [ -f "$HOME/.vimrc" ]; then
+        mv ~/.vimrc ~/.vimrc.`date +%Y%m%d`
+fi
+    
+echo "create new vimrc..."
+ln -s ~/.vim/vimrc ~/.vimrc
+    
+git submodule init
+git submodule update
+    
+vi +BundleInstall! +qall
